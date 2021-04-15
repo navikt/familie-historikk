c9b6d848b3a8b6e7e276682ac8ca7874d96d077c
Saikat Halder <saikat.halder@nav.no>
2021-04-15 16:39:29 +0200

    initial nais.yaml konfigurasjon med database,kafka. Kopierte Flyway,Swagger og logg konfigurasjon fra familie-tilbake

---
+++ src/main/kotlin/no/nav/familie/historikk/config/ApplicationConfig.kt
@@ -0,0 +1,48 @@
+package no.nav.familie.historikk.config
+
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import no.nav.familie.http.config.RestTemplateAzure
+import no.nav.familie.log.filter.LogFilter
+import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Import
+
+@SpringBootConfiguration
+@ComponentScan(ApplicationConfig.pakkenavn, "no.nav.familie.sikkerhet")
+@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger"])
+@Import(RestTemplateAzure::class)
+@EnableOAuth2Client(cacheEnabled = true)
+@ConfigurationPropertiesScan
+class ApplicationConfig {
+
+
+    @Bean
+    fun servletWebServerFactory(): ServletWebServerFactory {
+        val serverFactory = JettyServletWebServerFactory()
+        serverFactory.port = 8030
+        return serverFactory
+    }
+
+    @Bean
+    fun logFilter(): FilterRegistrationBean<LogFilter> {
+        val filterRegistration = FilterRegistrationBean<LogFilter>()
+        filterRegistration.filter = LogFilter()
+        filterRegistration.order = 1
+        return filterRegistration
+    }
+
+    @Bean
+    fun kotlinModule(): KotlinModule = KotlinModule()
+
+    companion object {
+
+        const val pakkenavn = "no.nav.familie.historikk"
+    }
+}
---
+++ src/main/kotlin/no/nav/familie/historikk/config/DatabaseConfig.kt
@@ -0,0 +1,30 @@
+package no.nav.familie.historikk.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.jdbc.datasource.DataSourceTransactionManager
+import org.springframework.transaction.PlatformTransactionManager
+import javax.sql.DataSource
+
+
+@Configuration
+@EnableJdbcAuditing
+@EnableJdbcRepositories("no.nav.familie.historikk")
+class DatabaseConfig : AbstractJdbcConfiguration() {
+
+    @Bean
+    fun operations(dataSource: DataSource): NamedParameterJdbcOperations {
+        return NamedParameterJdbcTemplate(dataSource)
+    }
+
+
+    @Bean
+    fun transactionManager(dataSource: DataSource): PlatformTransactionManager {
+        return DataSourceTransactionManager(dataSource)
+    }
+}
