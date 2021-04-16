26ec2faa024221066710a586db466a0ed1366aa0
Saikat Halder <saikat.halder@nav.no>
2021-04-16 10:06:17 +0200

    endret kafka konfig for å tillate familie-historikk som eier. Brukte ghcr.io istedenfor docker.pkg.github.com

--- src/main/kotlin/no/nav/familie/historikk/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/familie/historikk/config/DatabaseConfig.kt
@@ -1,7 +1,10 @@
 package no.nav.familie.historikk.config
 
+import no.nav.familie.historikk.common.repository.Endret
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
+import org.springframework.data.jdbc.core.convert.JdbcCustomConversions
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
@@ -9,6 +12,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
 import org.springframework.transaction.PlatformTransactionManager
+import java.util.Optional
 import javax.sql.DataSource
 
 
@@ -26,5 +30,17 @@ class DatabaseConfig : AbstractJdbcConfiguration() {
     @Bean
     fun transactionManager(dataSource: DataSource): PlatformTransactionManager {
         return DataSourceTransactionManager(dataSource)
+    }
+
+    @Bean
+    fun auditSporbarEndret(): AuditorAware<Endret> {
+        return AuditorAware {
+            Optional.of(Endret())
+        }
+    }
+
+    @Bean
+    override fun jdbcCustomConversions(): JdbcCustomConversions {
+        return JdbcCustomConversions()
     }
 }
---
+++ src/test/kotlin/no/nav/familie/historikk/LauncherLocal.kt
@@ -0,0 +1,19 @@
+package no.nav.familie.historikk
+
+import no.nav.familie.historikk.config.ApplicationConfig
+import no.nav.familie.historikk.database.DbContainerInitializer
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+@SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
+class LauncherLocal
+
+fun main(args: Array<String>) {
+    System.setProperty("spring.profiles.active",
+                       "local, mock-pdl, mock-oauth") //QAD hack for å få riktige profiler til spring 2.4.3
+    SpringApplicationBuilder(ApplicationConfig::class.java)
+            .initializers(DbContainerInitializer())
+            .profiles("local", "mock-pdl", "mock-oauth")
+            .run(*args)
+}
