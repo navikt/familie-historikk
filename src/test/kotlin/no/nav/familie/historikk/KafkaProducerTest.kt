ec2be4c72d172c1bfd46f5d107dee7e3d07b9431
Henning Solberg <henning.solberg@nav.no>
2021-05-05 16:21:48 +0200

    Setter opp embedded kafka ved lokal kjøring også

--- src/test/kotlin/no/nav/familie/historikk/KafkaProducerTest.kt
+++ src/test/kotlin/no/nav/familie/historikk/KafkaProducerTest.kt
@@ -10,7 +10,7 @@ import org.springframework.kafka.test.context.EmbeddedKafka
 import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 
-@EmbeddedKafka(partitions = 1, brokerProperties = ["listeners=PLAINTEXT://localhost:9092", "port=9092"])
+
 internal class KafkaProducerTest : OppslagSpringRunnerTest() {
 
     @Autowired
--- src/test/kotlin/no/nav/familie/historikk/config/KafkaLokalConfig.kt
+++ src/test/kotlin/no/nav/familie/historikk/config/KafkaLokalConfig.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.historikk.config
 
 import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.context.annotation.Bean
@@ -8,8 +9,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.config.TopicBuilder
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.test.EmbeddedKafkaBroker
 
 
 @Configuration
@@ -17,6 +20,19 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 @Profile("local", "integrasjonstest")
 class KafkaLokalConfig {
 
+
+    @Bean fun broker(): EmbeddedKafkaBroker? {
+        return EmbeddedKafkaBroker(1)
+                .kafkaPorts(9092)
+                .brokerProperty("listeners", "PLAINTEXT://localhost:9092,REMOTE://localhost:9093")
+                .brokerProperty("advertised.listeners", "PLAINTEXT://localhost:9092,REMOTE://localhost:9093")
+                .brokerProperty("listener.security.protocol.map", "PLAINTEXT:PLAINTEXT,REMOTE:PLAINTEXT")
+                .brokerListProperty("spring.kafka.bootstrap-servers")
+    }
+
+    @Bean fun topic(): NewTopic? {
+        return TopicBuilder.name("teamfamilie.historikk-topikk").partitions(1).replicas(1).build()
+    }
 
     @Bean
     fun consumerFactory(): ConsumerFactory<String, String> {
