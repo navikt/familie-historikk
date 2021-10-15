dd822e0fd05848fb8b7650dafb69f8a1a0fd8b69
haldersaikat <saikat.halder@nav.no>
2021-10-15 10:38:01 +0200

    la til skript for å få database tilgang i Prepod og Prod

--- src/main/kotlin/no/nav/familie/historikk/config/FlywayConfig.kt
+++ src/main/kotlin/no/nav/familie/historikk/config/FlywayConfig.kt
@@ -1,11 +1,13 @@
 package no.nav.familie.historikk.config
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
 
 @ConstructorBinding
 data class FlywayConfig(private val role: String) {
@@ -13,10 +15,19 @@ data class FlywayConfig(private val role: String) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     @Bean
-    fun flywayConfig(): FlywayConfigurationCustomizer {
+    fun flywayConfig(@Value("\${spring.flyway.placeholders.ignoreIfProd}") ignoreIfProd: String,
+                     environment: Environment): FlywayConfigurationCustomizer {
         logger.info("DB-oppdateringer kjøres med rolle $role")
+        val isProd = environment.activeProfiles.contains("prod")
+        val ignore = ignoreIfProd == "--"
         return FlywayConfigurationCustomizer {
             it.initSql(String.format("SET ROLE \"%s\"", role))
+            if (isProd && !ignore) {
+                throw RuntimeException("Prod profile-en har ikke riktig verdi for placeholder ignoreIfProd=$ignoreIfProd")
+            }
+            if (!isProd && ignore) {
+                throw RuntimeException("Profile=${environment.activeProfiles} har ignoreIfProd=false")
+            }
         }
     }
 }
