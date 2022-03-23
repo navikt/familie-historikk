f5053eb0cc9f2fa943184db2e453af65bb059b45
haldersaikat <saikat.halder@nav.no>
2022-03-23 14:29:35 +0100

    Fikset kafka avhengighet, oppgradertre spring boot til 2.6.4 og endret swagger til openApi

--- src/main/kotlin/no/nav/familie/historikk/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/historikk/config/ApplicationConfig.kt
@@ -22,7 +22,7 @@ import java.time.temporal.ChronoUnit
 
 @SpringBootConfiguration
 @ComponentScan(ApplicationConfig.pakkenavn, "no.nav.familie.sikkerhet")
-@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger"])
+@EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
 @ConfigurationPropertiesScan
 class ApplicationConfig {
 
