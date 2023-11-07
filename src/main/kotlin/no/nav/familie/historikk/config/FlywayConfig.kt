0bf8807be25dbf85a0ac67ce94367362997d893a
Henning Solberg <henning.solberg@nav.no>
2023-11-07 15:13:03 +0100

    bumper til spring 3

--- src/main/kotlin/no/nav/familie/historikk/config/FlywayConfig.kt
+++ src/main/kotlin/no/nav/familie/historikk/config/FlywayConfig.kt
@@ -1,33 +1,18 @@
 package no.nav.familie.historikk.config
 
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
+import org.flywaydb.core.api.configuration.FluentConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer
-import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
-import org.springframework.core.env.Environment
+import org.springframework.context.annotation.Profile
 
-@ConstructorBinding
-data class FlywayConfig(private val role: String) {
-
-    private val logger = LoggerFactory.getLogger(this::class.java)
-
+@Profile("!dev")
+@ConditionalOnProperty("spring.flyway.enabled")
+class FlywayConfig(private val role: String) {
     @Bean
-    fun flywayConfig(
-        @Value("\${spring.flyway.placeholders.ignoreIfProd}") ignoreIfProd: String,
-        environment: Environment
-    ): FlywayConfigurationCustomizer {
-        logger.info("DB-oppdateringer kjøres med rolle $role")
-        val isProd = environment.activeProfiles.contains("prod")
-        val ignore = ignoreIfProd == "--"
-        return FlywayConfigurationCustomizer {
-            it.initSql(String.format("SET ROLE \"%s\"", role))
-            if (isProd && !ignore) {
-                throw RuntimeException("Prod profile-en har ikke riktig verdi for placeholder ignoreIfProd=$ignoreIfProd")
-            }
-            if (!isProd && ignore) {
-                throw RuntimeException("Profile=${environment.activeProfiles} har ignoreIfProd=false")
-            }
+    fun flywayConfig(): FlywayConfigurationCustomizer {
+        return FlywayConfigurationCustomizer { c: FluentConfiguration ->
+            c.initSql("SET ROLE \"$role\"")
         }
     }
 }
